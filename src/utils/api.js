a39602fa01851b2c7380cb3cb2fc746b8d7243b3
jms-ry <jamesroyxd30@gmail.com>
2026-03-23 10:39:09 +0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,6 +6,7 @@ export async function sendMessage(systemPrompt, messages) {
   })
 
   const data = await response.json()
+  console.log('API response:', data)
 
   if (!response.ok) {
     throw new Error(data.error || 'Something went wrong')
